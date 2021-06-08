f785a5cd87d9b3a7a2d4c29967591d0f1e8eac7a
Viktor Kadza <viktorkadza@gmail.com>
2021-06-08 22:57:15 +0200

    Update layout.js
    cc

--- src/components/layout.js
+++ src/components/layout.js
@@ -41,7 +41,7 @@ const Layout = ({ children }) => {
         >
           © {new Date().getFullYear()}, Built with
           {` `}
-          <a href="https://www.gatsbyjs.com">Gatsby</a>
+          <a href="https://www.gatsbyjs.com">VSCODE</a>
         </footer>
       </div>
     </>
